7d1e35f45505480a62a58029916e69fccdc00a9b
Maulana Ghozi Humama <ghozihz@gmail.com>
2019-07-09 17:01:44 +0700

    add function urutAbjad

---
+++ exercise-7.js
@@ -0,0 +1,14 @@
+function urutkanAbjad(str) {
+    // you can only write your code here!
+    let newStr = str.split('');
+    newStr.sort();
+    let result = newStr.join('');
+    return result;
+  }
+  
+  // TEST CASES
+  console.log(urutkanAbjad('hello')); // 'ehllo'
+  console.log(urutkanAbjad('truncate')); // 'acenrttu'
+  console.log(urutkanAbjad('developer')); // 'deeeloprv'
+  console.log(urutkanAbjad('software')); // 'aeforstw'
+  console.log(urutkanAbjad('aegis')); // 'aegis'
