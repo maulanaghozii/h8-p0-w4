d175978c2d3a4251c7d220eab761de597a6c36b4
Maulana Ghozi Humama <ghozihz@gmail.com>
2019-07-10 15:02:48 +0700

    add function changeMe

---
+++ exercise-10.js
@@ -0,0 +1,33 @@
+function changeMe(arr) {
+    // you can only write your code here!
+    for(i=0; i<arr.length; i++){
+        let ageNow;
+        if(arr[i][3]===undefined || arr[i][3]>2019){
+            ageNow = 'Invalid Birth Year';
+        } else{
+            ageNow = 2019 - arr[i][3];
+        }
+        let objIdentity = {
+            firstName: arr[i][0],
+            lastName : arr[i][1],
+            gender   : arr[i][2],
+            age      : ageNow,
+        }
+        console.log(i+1+'. '+objIdentity.firstName+' '+objIdentity.lastName+':');
+        console.log(objIdentity);
+    }
+    
+}
+  
+  // TEST CASES
+  changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); // 1. Christ Evans:
+  // { firstName: 'Christ',
+  //   lastName: 'Evans',
+  //   gender: 'Male',
+  //   age: 37 }
+  // 2. Robert Downey:
+  // { firstName: 'Robert',
+  //   lastName: 'Downey',
+  //   gender: 'Male',
+  //   age: 'Invalid Birth Year' }
+  changeMe([]); // ""
