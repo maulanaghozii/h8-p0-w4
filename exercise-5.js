73b235b7acb18609ee41b1cc9dcbad9da9e8429e
Maulana Ghozi Humama <ghozihz@gmail.com>
2019-07-11 14:45:59 +0700

    add function ubahHuruf

--- exercise-5.js
+++ exercise-5.js
@@ -1,10 +1,22 @@
 function ubahHuruf(kata) {
     // you can only write your code here!
-    
+    const abjad = 'abcdefghijklmnopqrstuvwxyz'; //untuk memudahkan ubah huruf selanjutnya kumpulkan semua abjad
+    let arrKata = kata.split('');
+
+    for(let index=0; index<arrKata.length; index++){
+      if(abjad.indexOf(arrKata[index])!== -1){
+        if(arrKata[index] === 'z'){
+          arrKata[index] = 'a';
+        } else{
+          arrKata[index] = abjad[abjad.indexOf(arrKata[index])+1]
+        }
+      } 
+    }
+    return arrKata.join('');
   }
   
   // TEST CASES
-  console.log(ubahHuruf('wow')); // xpx
+  console.log(ubahHuruf('woz')); // xpx
   console.log(ubahHuruf('developer')); // efwfmpqfs
   console.log(ubahHuruf('javascript')); // kbwbtdsjqu
   console.log(ubahHuruf('keren')); // lfsfo
