153499334a31302f03e997f56c84f6ca8ec88e95
Maulana Ghozi Humama <ghozihz@gmail.com>
2019-07-12 15:50:35 +0700

    add sunction digitPerkalianMinimum

--- exercise-6.js
+++ exercise-6.js
@@ -1,5 +1,25 @@
 function digitPerkalianMinimum(angka) {
     // you can only write your code here!
+    let shortestDigit = 0; //untuk menampung digit terpendek perkalian 
+    //lakukan perulangan untuk mencari faktorial
+    for(let i=1; i<=angka;i++){ //faktorial pertama
+      for(let j=1; j<=angka;j++){ //faktorail kedua
+        if(i*j === angka){ 
+          let faktorialA = i.toString();
+          let faktorialB = j.toString();
+          let mergerFaktorial = faktorialA+faktorialB;
+          let lenghtMerger = mergerFaktorial.length;
+
+          if(shortestDigit === 0){
+            shortestDigit = lenghtMerger;
+          } else if(shortestDigit > lenghtMerger){
+            shortestDigit = lenghtMerger;
+          }
+
+        }
+      }
+    }
+    return shortestDigit;
   }
   
   // TEST CASES
